6deaf00f69ad93c6b1dc81b0529b58f73019a869
Jonathan Dent <jonathan.dent@bristolitcompany.com>
2021-05-18 15:12:01 +0100

    Exposing grid component so it can be used directly.

--- src/pact-vue-grid/src/index.ts
+++ src/pact-vue-grid/src/index.ts
@@ -2,3 +2,4 @@ export * from "./models";
 export * from "./crudGrid";
 export * from "./childParentCRUDGrid";
 export { default as CRUDPageComponent } from "./components/Page.vue";
+export { default as CRUDGridComponent } from "./components/Grid.vue";
