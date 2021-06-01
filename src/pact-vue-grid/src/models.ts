4d60a076298d1f05a542b07da4ae156452e2048f
Jonathan Dent <jonathan.dent@bristolitcompany.com>
2021-06-01 17:53:26 +0100

    Trying out true or undefined in options.

--- src/pact-vue-grid/src/models.ts
+++ src/pact-vue-grid/src/models.ts
@@ -11,7 +11,7 @@ export interface GridOptions {
     filters?: GridCascadeFilter[];
     customActions?: GridCustomAction[];
     parent?: number;
-    selectable: boolean;
+    selectable: true | undefined;
     selectionMode?: GridSelectionMode;
 }
 
