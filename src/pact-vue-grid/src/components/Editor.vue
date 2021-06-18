f844281184e4779dbe351809fbf745d95bf576fc
Jonathan Dent <jonathan.dent@bristolitcompany.com>
2021-06-18 14:08:32 +0100

    Fixing up padding on markdown editor control #18

--- src/pact-vue-grid/src/components/Editor.vue
+++ src/pact-vue-grid/src/components/Editor.vue
@@ -81,7 +81,7 @@
 							:pattern="field.pattern"
 						/>
 					</div>
-					<div v-if="field.type == 'markdown'" class="col-12">
+					<div v-if="field.type == 'markdown'" class="col-12 mb-3">
 						<div class="row">
 							<div class="col-12">
 								<label :for="field.name" class="form-label">{{ field.display }}</label>
--- src/pact-vue-grid/src/index.ts
+++ src/pact-vue-grid/src/index.ts
@@ -4,4 +4,5 @@ export * from "./childParentCRUDGrid";
 export { default as CRUDPageComponent } from "./components/Page.vue";
 export { default as CRUDGridPageComponent } from "./components/GridPage.vue";
 export { default as CRUDGridComponent } from "./components/Grid.vue";
+export { default as CRUDEditorComponent } from "./components/Editor.vue";
 export { default as Pagination } from "./components/Pagination.vue";
