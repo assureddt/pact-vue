2b3fe5dad8cd8af363f552c298957725948affe0
Lee Jones <lee.jones@assureddigitaltech.com>
2023-01-26 13:15:16 +0000

    Building up sample page

--- src/pact-vue-grid/src/index.ts
+++ src/pact-vue-grid/src/index.ts
@@ -1,9 +1,69 @@
-export * from "./models";
-export * from "./crudGrid";
-export * from "./childParentCRUDGrid";
-export { default as CRUDPageComponent } from "./components/PactPage.vue";
-export { default as CRUDGridPageComponent } from "./components/GridPage.vue";
-export { default as CRUDGridComponent } from "./components/PactGrid.vue";
-export { default as CRUDEditorComponent } from "./components/GridEditor.vue";
-export { default as Pagination } from "./components/GridPagination.vue";
-export { default as CascadeFilters } from "./components/CascadeFilters.vue";
+import "bootstrap";
+import { CRUDGrid, GridOrderDirection } from "./main";
+
+import "bootstrap/scss/bootstrap.scss";
+
+//Fortawesome
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { faPlus } from "@fortawesome/pro-solid-svg-icons/faPlus";
+import { faEdit } from "@fortawesome/pro-solid-svg-icons/faEdit";
+import { faTimes } from "@fortawesome/pro-solid-svg-icons/faTimes";
+import { faBan } from "@fortawesome/pro-solid-svg-icons/faBan";
+import { faSave } from "@fortawesome/pro-solid-svg-icons/faSave";
+import { faCheck } from "@fortawesome/pro-solid-svg-icons/faCheck";
+
+library.add(faPlus, faEdit, faTimes, faBan, faSave, faCheck);
+dom.watch();
+
+new CRUDGrid("#app", {
+	gridOptions: {
+		read: "/read.json",
+		delete: "/remove",
+		pageSize: 10,
+        order: {
+            columnName : "name",
+            direction: GridOrderDirection.ascending
+        },
+		allowEdit: true,
+		allowAdd: true,
+		allowDelete: true,
+		deleteColumn: "name",
+		buttonsWidth: 79
+	},
+	gridColumns: [
+		{
+			name: "name",
+			display: "Name",
+            type: "text"
+		},
+		{
+			name: "description",
+			display: "Description",
+            type: "text"
+		},
+	],
+	editOptions: {
+		add: "/add",
+		edit: "/edit",
+		grabData: "/data",
+		editTitle: "Edit",
+		addTitle: "Add"
+	},
+	editFields: [
+		{
+			name: "name",
+			display: "Name",
+			placeholder: "Name",
+			required: true,
+            type: "text"
+		},
+		{
+			name: "description",
+			display: "Description",
+			placeholder: "Description",
+			required: true,
+            type: "text"
+		}
+	],
+	pageTitle: "Things"
+});
--- src/pact-vue-grid/vite.config.js
+++ src/pact-vue-grid/vite.config.js
@@ -1,4 +1,4 @@
-import {defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 const path = require("path");
@@ -6,7 +6,7 @@ const path = require("path");
 export default defineConfig({
     build: {
         lib: {
-            entry: path.resolve(__dirname, 'src/index.ts'),
+            entry: path.resolve(__dirname, 'src/main.ts'),
             name: '@assureddt/pact-vue-grid',
             fileName: (format) => `pact-vue-grid.${format}.js`
         }
@@ -15,6 +15,18 @@ export default defineConfig({
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src")
+        }
+    },
+    rollupOptions: {
+        // make sure to externalize deps that shouldn't be bundled
+        // into your library
+        external: ['vue'],
+        output: {
+            // Provide global variables to use in the UMD build
+            // for externalized deps
+            globals: {
+                vue: 'Vue'
+            }
         }
     }
 });
